d72429aaecc5ff37cc537d650ff04a021f0e7ed2
Akseli Palmer <akseli@toddsden.com>
2024-10-21 21:06:33 -0400

    getting the heros by using an observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,9 +8,16 @@ import { HEROES } from '../mock-heroes';
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+  constructor(private heroService: HeroService) {}
+  getHeros(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+  ngOnInit(): void {
+    this.getHeros();
   }
 }
